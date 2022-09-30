b8fe815e290400a48b0211d3d6c20feb351f66fa
Hjayej Mohamed <mohamedhjaij800@gmail.com>
2022-09-30 21:04:58 +0200

    feat: add profile model and signals

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
-class UserAdmin (BaseUserAdmin):
-    ordering = ['email']
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model =User
+    model = User
     list_display = [
         "pkid",
         "id",
@@ -20,41 +21,59 @@ class UserAdmin (BaseUserAdmin):
         "is_staff",
         "is_active",
     ]
-    list_display_links = ['id','email']
-    list_filter = ['email','first_name','last_name','is_staff','is_active' ]
+    list_display_links = ["id", "email"]
+    list_filter = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    ]
     fieldsets = (
         (
             _("Login Credentials"),
-            {"fields":("email","password",)},
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
         ),
         (
-            _("Personnal Information"),
-            {"fields":("username","first_name","last_name",)},
+            _("Personal Information"),
+            {
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                )
+            },
         ),
         (
             _("Permissions and Groups"),
-            {"fields":("is_active",
-                       "is_staff",
-                       "is_superuser"
-                       "groups",
-                       "user_permissions",
-                       )},  
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
         ),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
         (
-            _("Important Dates"),
-            {"fields":("last_login","date_joined")},
-            
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
         ),
     )
-    add_fieldsets=(
-            (
-                None,
-                {
-                "classes":("wide",),
-                "fields":("email","password","password2","is_staff","is_active"),
-                },
-            )
-    )
-    
-    search_fields =["email","username","first_name","last_name"]
-admin.site.register(User,UserAdmin)
+    search_fields = ["email", "username", "first_name", "last_name"]
+
+
+admin.site.register(User, UserAdmin)
